701e13f0a5ea2d95949673c86918e1b2c00aac95
Helal Anwar <hilal123anwar@gmail.com>
2023-01-29 14:12:23 +0530

    Added new questions

---
+++ src/util/Parcel.java
@@ -0,0 +1,17 @@
+package util;
+
+import java.util.Scanner;
+
+public class Parcel {
+public static void main(String[] args) {
+ Scanner in=new Scanner(System.in);
+ System.out.println("Enter the mass in gram");
+ int mass=in.nextInt();
+ double charger=0.0;
+ if(mass<=500)
+   charger=500;
+ else
+    charger=500+(mass-500)/200.0*25;
+System.out.println("Total charge = "+charger);
+    }
+}
